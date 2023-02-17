cfa25b23d46d9b0e2b98f31a51f7a2fe8a19ae50
Sivert <sivertgh@stud.ntnu.no>
2023-02-17 20:12:41 +0100

    fix tailwind issues

--- src/styles/tailwind.js
+++ src/styles/tailwind.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/@gryt/**/*.tsx"
+    "pages/**/*.{js,ts,jsx,tsx}",
+    "node_modules/@gryt/**/*.tsx",
   ],
   theme: {
     extend: {},
