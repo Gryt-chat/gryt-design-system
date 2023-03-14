1075e3db63bf68fe0ba0dd7dddc2d331fd061c4e
Sivert <sivertgh@stud.ntnu.no>
2023-03-14 23:11:43 +0100

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,5 @@
-import fs from 'fs'
-import path from 'path'
-
-const components: { [key: string]: any } = {}
-
-// Read all component files in the components directory
-const componentFiles = fs.readdirSync(path.join(__dirname, 'components'))
-
-// Loop through each file and export all named exports
-for (const file of componentFiles) {
-  if (file.endsWith('.tsx')) {
-    const componentModule = require(path.join(__dirname, 'components', file))
-    for (const key in componentModule) {
-      if (Object.prototype.hasOwnProperty.call(componentModule, key)) {
-        components[key] = componentModule[key]
-      }
-    }
-  }
-}
-
-export { components }
+export * from './components/badge/Badge'
+export * from './components/button/Button'
+export * from './components/modal/Modal'
+export * from './components/serverCard/ServerCard'
+export * from './components/user/avatar/Avatar'
