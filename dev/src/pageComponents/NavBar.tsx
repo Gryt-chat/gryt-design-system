d8049fa305f8c13b5852cd951931f3954b38ca11
Sivert <sivertgh@stud.ntnu.no>
2023-06-18 14:49:29 +0200

    Implemented navbar

--- dev/src/pageComponents/NavBar.tsx
+++ dev/src/pageComponents/NavBar.tsx
@@ -11,11 +11,13 @@ export function NavBar() {
   }, [])
 
   return (
-    <nav className='fixed inset-0 right-auto w-96 bg-base-300 pt-12 flex flex-col gap-6 items-center'>
-      <Link href='/'>
-        <h2 className='font-bold text-xl text-primary'>Gryt UI</h2>
-      </Link>
-      {menuItems}
+    <nav className='fixed inset-0 right-auto w-96 bg-base-300 pt-12 flex flex-col items-center'>
+      <div className='flex flex-col gap-6 items-start'>
+        <Link href='/'>
+          <h2 className='font-bold text-xl text-primary'>Gryt UI</h2>
+        </Link>
+        {menuItems}
+      </div>
     </nav>
   )
 }
--- dev/src/utils/GenerateMenu.tsx
+++ dev/src/utils/GenerateMenu.tsx
@@ -1,42 +1,119 @@
 import Link from 'next/link'
+import React from 'react'
+import navbarNames from '../navbar_names.json'
 
-interface jsonDataInterface {
-  [key: string]: string
+interface jsonData {
+  [key: string]: any
 }
 
-export function GenerateMenu(jsonData: jsonDataInterface) {
-  const groups: { [key: string]: any } = {}
+interface TransformedData {
+  [key: string]: any
+  URL?: string
+}
 
-  Object.keys(jsonData).forEach((key) => {
-    const dir = jsonData[key]
-    const parts = dir.split('/')
-    if (parts[1].length > 0 && !groups[parts[1]]) groups[parts[1]] = []
-    if (key.length > 0) {
-      groups[parts[1]]?.push({ [key]: dir })
+const ConvertJsonData = (data: jsonData): TransformedData => {
+  const transformedData: TransformedData = {
+    app: { URL: data.app }
+  }
+
+  for (const key in data) {
+    if (key === 'app') continue
+
+    const value = data[key]
+    const parts = value.split('/').filter(Boolean)
+    let currentObj: any = transformedData
+
+    for (let i = 0; i < parts.length; i++) {
+      const part = parts[i]
+
+      if (!currentObj[part]) {
+        currentObj[part] = { URL: `/${parts.slice(0, i + 1).join('/')}` }
+      }
+
+      currentObj = currentObj[part]
+    }
+  }
+
+  return transformedData
+}
+
+function NavLink({
+  children,
+  size
+}: {
+  children: React.ReactNode
+  size: number
+}) {
+  return (
+    <h2
+      className={`${
+        size === 1
+          ? 'text-2xl font-bold text-primary'
+          : size <= 2
+          ? 'text-xl font-medium'
+          : 'text-secondary ml-4'
+      }`}
+    >
+      {children}
+    </h2>
+  )
+}
+
+function checkData(
+  data: TransformedData,
+  currentName?: string,
+  currentLevel = 0
+) {
+  const level = currentLevel + 1 // see if top level or not
+
+  return Object.keys(data).map((level1Key) => {
+    const name =
+      currentName !== undefined
+        ? navbarNames[level1Key as keyof typeof navbarNames]
+        : currentName ?? level1Key
+
+    // If has children > 1 (aka a category🤓)
+    if (
+      typeof data[level1Key] === 'object' &&
+      Object.keys(data[level1Key]).length > 1
+    ) {
+      return (
+        <li className='flex flex-col gap-1' key={level1Key} id={name as string}>
+          {level > 1 && (
+            <Link href={data[level1Key].URL || '/'}>
+              <NavLink size={level}>{name}</NavLink>
+            </Link>
+          )}
+          <ul className={`flex flex-col ${level === 1 ? 'gap-4' : 'gap-1'}`}>
+            {checkData(data[level1Key], name, level)}
+          </ul>
+        </li>
+      )
+    }
+    // If has children == 1 (aka a link🤓)
+    else if (
+      typeof data[level1Key] === 'object' &&
+      Object.keys(data[level1Key]).length === 1
+    ) {
+      return (
+        level > 1 && (
+          <li key={level1Key} id={`${name} URL`}>
+            <Link href={data[level1Key].URL || '/'}>
+              <NavLink size={level}>{name}</NavLink>
+            </Link>
+          </li>
+        )
+      )
     }
   })
-  return Object.keys(groups).map((categoryKey) => {
-    return (
-      <div key={categoryKey}>
-        <h2 className='font-bold text-2xl'>{categoryKey}</h2>
-        <ul className='flex flex-col gap-2'>
-          {groups[categoryKey].map((element: { [key: string]: any }) => {
-            return Object.keys(element).map((categoryItemKey) => {
-              if (categoryItemKey !== categoryKey)
-                return (
-                  <li key={categoryItemKey}>
-                    <Link
-                      className='link font-medium'
-                      href={element[categoryItemKey]}
-                    >
-                      {categoryItemKey}
-                    </Link>
-                  </li>
-                )
-            })
-          })}
-        </ul>
-      </div>
-    )
-  })
+}
+
+function GenerateNavbarList(data: TransformedData) {
+  return <ul className='flex flex-col gap-4'>{checkData(data)}</ul>
+}
+
+export function GenerateMenu(jsonData: jsonData) {
+  const menuJson = ConvertJsonData(jsonData)
+
+  return GenerateNavbarList(menuJson)
 }
