f826dd5354c2fdf7a3ae65fd3a5c16762e631d0f
Sivert Gullberg Hansen <sivert-gullberg.hansen@norsk-tipping.no>
2023-11-19 22:02:17 +0100

    Added dev page

---
+++ dev/src/app/dev/page.tsx
@@ -0,0 +1,16 @@
+import ButtonV2 from '@/components/button/ButtonV2'
+import Input from '@/components/input/Input'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='fixed left-0 right-0 bottom-0 top-0 flex flex-col gap-4 items-center justify-center'>
+      {/* <ButtonV2 className='bg-white text-black'>Blank</ButtonV2>
+      <ButtonV2.Primary loading>Primary</ButtonV2.Primary>
+      <ButtonV2.Secondary>Secondary</ButtonV2.Secondary>
+      <ButtonV2.Accent>Accent</ButtonV2.Accent> */}
+      {/* <Input />
+      <Input inputMode='email' header='Email'/> */}
+    </div>
+  )
+}
